058fed054136bfff4415ad1a415dec6168f3cd6d
Roland Foucher <roland.foucher.pro@gmail.com>
2021-12-08 16:22:40 +0100

    attack choice drag on player / delete init

--- src/main/java/com/example/simplon/promo16/players/Match.java
+++ src/main/java/com/example/simplon/promo16/players/Match.java
@@ -1,17 +1,30 @@
 package com.example.simplon.promo16.players;
 
+
 import com.example.simplon.promo16.perso.Perso;
 
 public class Match {
     private Player player1;
     private Player player2;
     int persoSelected = 0;
+    
 
     public void match(){
         while(!player1.playerLoose() || !player2.playerLoose() ){
-           Perso player1Perso = player1.getPlayerPerso(persoSelected);
-           Perso player2PersoToAttack = player2.getPlayerPerso(persoSelected);
+           Perso player1Perso = player1.getIndividualPlayerPerso(persoSelected);
+           Perso player2PersoToAttack = player2.getIndividualPlayerPerso(persoSelected);
         }
+    }
+  
+
+
+
+    public int getPersoSelected() {
+        return persoSelected;
+    }
+
+    public void setPersoSelected(int persoSelected) {
+        this.persoSelected = persoSelected;
     }
 
 }
