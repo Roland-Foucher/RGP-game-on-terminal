d0e1da9456d5850e5dd46b1db0dcf739caa97a6b
Roland Foucher <roland.foucher.pro@gmail.com>
2021-12-09 17:51:34 +0100

    implement run program, playerturn

--- src/main/java/com/example/simplon/promo16/game/Match.java
+++ src/main/java/com/example/simplon/promo16/game/Match.java
@@ -14,20 +14,57 @@ public class Match {
     private Player player1;
     private Player player2;
     private int personnageInitChoice[] = new int[4];
+    Display display = new Display();
     
 
     public void runProgramme(){
-        int personnageChoice;
-        Display display = new Display();
+        
+        
         display.init();
-    
-        personnageInitChoice[0] = display.personnageChoice();
-        personnageInitChoice[2] = display.personnageChoice();
-        personnageInitChoice[3] = display.personnageChoice();
-        personnageInitChoice[4] = display.personnageChoice();
+        
+        //choose personnages
+        personnageInitChoice[0] = display.personnageChoice("premier", "player1");
+        personnageInitChoice[1] = display.personnageChoice("second", "player1");
+        personnageInitChoice[2] = display.personnageChoice("troisieme", "player1");
+        personnageInitChoice[3] = display.personnageChoice("quatrieme", "player1");
+        player1 = this.makePlayer(personnageInitChoice[0], personnageInitChoice[1], personnageInitChoice[2], personnageInitChoice[3], "Player1");
+        
+        personnageInitChoice[0] = display.personnageChoice("premier", "player2");
+        personnageInitChoice[1] = display.personnageChoice("second", "player2");
+        personnageInitChoice[2] = display.personnageChoice("troisieme", "player2");
+        personnageInitChoice[3] = display.personnageChoice("quatrieme", "player2");
+        player2 = this.makePlayer(personnageInitChoice[0], personnageInitChoice[1], personnageInitChoice[2], personnageInitChoice[3], "Player2");
+        
+        while(!player1.playerLoose() || !player2.playerLoose()){
+            this.playerTurn(player1, player2);
+            this.playerTurn(player2, player1);
+        }
+        if (player1.playerLoose()){
+            System.out.println(player2.getPlayerName() +  " win !!");
+        }else{
+            System.out.println(player1.getPlayerName() +  " win!!");
+        }
+
+
 
     }
    
+    public void playerTurn(Player playerTurn , Player playerAdvers){
+
+        int persoSelected = display.playerChoosePerso(playerTurn);
+        int playerChooseWhatToDo = display.playerChooseWhatToDo(playerTurn);
+            if (playerChooseWhatToDo == 1){
+                int playerChooseAttack = display.playerChooseAttack(playerTurn, playerTurn.getIndividualPlayerPerso(persoSelected));
+                int playerChooseAdversToAttack = display.playerChooseAdversToAttack(playerAdvers);
+                player1.attackOption(persoSelected, playerChooseAttack, playerChooseAdversToAttack, playerAdvers);
+            }else if (playerChooseWhatToDo == 2){
+
+                int playerChooseCardOption = display.playerChooseCardOption();
+                player1.chooseCardOption(playerChooseCardOption, persoSelected);
+            }else{
+                System.out.println("error in the choose attack");
+            }
+    }
     /**
      * make a player with the choose of user
      * @param userChoosePerso1 int choose by user for perso one
@@ -36,12 +73,12 @@ public class Match {
      * @param userChoosePerso4 int choose by user for perso four
      * @return player created
      */
-    public Player makePlayer(int userChoosePerso1, int userChoosePerso2, int userChoosePerso3, int userChoosePerso4){
+    public Player makePlayer(int userChoosePerso1, int userChoosePerso2, int userChoosePerso3, int userChoosePerso4, String name){
         Perso perso1 = this.choosePerso(userChoosePerso1);
         Perso perso2 = this.choosePerso(userChoosePerso2);
         Perso perso3 = this.choosePerso(userChoosePerso3);
         Perso perso4 = this.choosePerso(userChoosePerso4);
-        Player player = new Player(perso1, perso2, perso3, perso4);
+        Player player = new Player(perso1, perso2, perso3, perso4, name);
 
         return player;
     }
