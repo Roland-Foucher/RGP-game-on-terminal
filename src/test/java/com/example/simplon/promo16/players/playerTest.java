cb1b6a494febeb1a616d7866fbe5a6030377bcd4
Roland Foucher <roland.foucher.pro@gmail.com>
2021-12-09 14:47:53 +0100

    finish attackoption + testattack

--- src/test/java/com/example/simplon/promo16/players/playerTest.java
+++ src/test/java/com/example/simplon/promo16/players/playerTest.java
@@ -19,7 +19,12 @@ public class playerTest {
     Perso elfe;
     Perso necro;
     Perso knight;
+    Perso orc2;
+    Perso elfe2;
+    Perso necro2;
+    Perso knight2;
     Player player1;
+    Player player2;
 
     @BeforeEach
     public void init(){
@@ -27,7 +32,12 @@ public class playerTest {
         elfe = new Elfe();
         necro = new Necromancer();
         knight = new Knigth();
+        orc2 = new Orc();
+        elfe2 = new Elfe();
+        necro2 = new Necromancer();
+        knight2 = new Knigth();
         player1 = new Player(orc, elfe, necro, knight);
+        player2 = new Player(orc2, elfe2, necro2, knight2);
     }
     @AfterEach
     public void finish(){
@@ -36,9 +46,13 @@ public class playerTest {
         necro = null;
         knight = null;
         player1 = null;
+        orc2 = null;
+        elfe2 = null;
+        necro2 = null;
+        player2 = null;
     }
 
-    
+    //Player Loose Tests
     @Test
     void testPlayerLoose_WhenAllPersoDead() {
         for (int i = 0; i < 4; i++) {
@@ -56,11 +70,13 @@ public class playerTest {
         assertEquals(false, player1.playerLoose());
     }
 
-    @Test
+    //
+   @Test
     void testGetNumberOfPerso() {
         assertEquals(4, player1.getNumberOfPerso());
     }
 
+    //get Perso
     @Test
     void getIndividualPlayerPerso(){
         
@@ -71,5 +87,66 @@ public class playerTest {
     void getIndividualPlayerPerso_WithBadNumberInput(){
         assertEquals(null, player1.getIndividualPlayerPerso(10));
     }
+    //choice card test
+    @Test
+    void chooseCardOption_GetMana(){
+        player1.getIndividualPlayerPerso(1).setMana(-100);
+        player1.chooseCardOption(2, 2);
+        assertEquals(50, player1.getIndividualPlayerPerso(1).getMana());
+    }
+
+    @Test
+    void chooseCardOption_GetHealth(){
+        player1.getIndividualPlayerPerso(1).setHealth(-70);
+        player1.chooseCardOption(1, 2);
+        assertEquals(50, player1.getIndividualPlayerPerso(1).getHealth());
+    }
+
+    @Test
+    void chooseCardOption_InvalideNumber(){
+        player1.getIndividualPlayerPerso(1).setHealth(-70);
+        player1.chooseCardOption(3, 2);
+        assertEquals(10, player1.getIndividualPlayerPerso(1).getHealth());
+    }
+
+    @Test
+    void chooseCardOption_GetHealth_WhenPersoIsDead(){
+        player1.getIndividualPlayerPerso(1).setHealth(-200);
+        player1.chooseCardOption(1, 2);
+        assertEquals(0, player1.getIndividualPlayerPerso(1).getHealth());
+        assertEquals(false, player1.getIndividualPlayerPerso(1).isAlive());
+    }
+
+    @Test
+    void chooseCardOption_NotAnougthCard(){
+        player1.getIndividualPlayerPerso(1).setMana(-100);
+        player1.chooseCardOption(2, 2);
+        player1.getIndividualPlayerPerso(1).setMana(-100);
+        player1.chooseCardOption(2, 2);
+        player1.getIndividualPlayerPerso(1).setMana(-100);
+        player1.chooseCardOption(2, 2);
+        assertEquals(0, player1.getIndividualPlayerPerso(1).getMana());
+    }
+
+    //choice attack test
+    @Test 
+    void attackOption_chooseWeaponAttack(){
+        player1.attackOption(1, 1, 1, player2);
+        assertEquals(20, player2.getIndividualPlayerPerso(0).getHealth());
+    }
+
+    @Test
+    void attackOption_chooseManaAttack(){
+        player1.attackOption(1, 2, 1, player2);
+        assertEquals(80, player2.getIndividualPlayerPerso(0).getHealth());
+    }
+
+    @Test // TODO finir test attack option
+    void attackOption_invalidAttackNumber(){
+        player1.attackOption(1, 4, 1, player2);
+        assertEquals(120, player2.getIndividualPlayerPerso(0).getHealth());
+    }
+
+
 
 }
